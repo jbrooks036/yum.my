070e46f2fb40ca0008a75a80dd069c4ea2ee617a
JoAnn Brooks <joannbrooks036@gmail.com>
2014-09-15 12:49:42 -0500

    initial files

---
+++ client/index.js
@@ -0,0 +1,13 @@
+(function(){
+  'use strict';
+
+  angular.module('yum.my', ['ngRoute'])
+  .config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when('/', {templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
+    .when('/register', {templateUrl:'/views/register/register.html', controller:'RegisterCtrl'})
+    .when('/login', {templateUrl:'/views/login/login.html', controller:'LoginCtrl'})
+    .otherwise({redirectTo:'/'});
+  }]);
+})();
+
